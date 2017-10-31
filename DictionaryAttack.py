781dcae198c0669be06ec33cb593368c2420cd01
fionnmcguire1 <fionnmcguire1@gmail.com>
2017-10-31 18:56:49 +0000

    Reading in files to fuel the attack

---
+++ DictionaryAttack.py
@@ -0,0 +1,29 @@
+'''
+Author: Fionn Mcguire
+Date: 31-10-2017
+Description: Create a program to run a dictionary attack to test for brute force resistance
+'''
+
+
+#Not entirely sure how we're going to handle the input
+class dictAttack(object):
+    def __init__():
+        self.wordList = []
+        self.popularPassword = []
+        self.replacements = {}
+        
+    def checkValue
+
+    def readDictFile(self,filename):
+        with open(filename) as f:
+            for line in f:
+                self.wordList.append(line)
+
+    def readCommonPassFile(self,filename):
+        with open(filename) as f:
+            for line in f:
+                self.popularPassword.append(line)
+
+
+    def execAttack():
+
