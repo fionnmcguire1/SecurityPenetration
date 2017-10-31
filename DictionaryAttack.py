ce69dab10b8b54d5116cce21e0cc99dcc7559bd2
fionnmcguire1 <fionnmcguire1@gmail.com>
2017-10-31 19:23:20 +0000

    Creating a simple case scenario

--- DictionaryAttack.py
+++ DictionaryAttack.py
@@ -5,15 +5,13 @@ Description: Create a program to run a dictionary attack to test for brute force
 '''
 
 
-#Not entirely sure how we're going to handle the input
+
 class dictAttack(object):
     def __init__():
         self.wordList = []
         self.popularPassword = []
         self.replacements = {}
         
-    def checkValue
-
     def readDictFile(self,filename):
         with open(filename) as f:
             for line in f:
@@ -24,6 +22,26 @@ class dictAttack(object):
             for line in f:
                 self.popularPassword.append(line)
 
+    #These are a series of True False variables used to denote the extent of the attack
+    def execAttack(self,replacements,numbers,caseSensitive,engageFullDict):
+        
+        #Always try most common passwords
+        readCommonPassFile('DataSets/CommonPassword.txt')
 
-    def execAttack():
+        if engageFullDict == True:
+            readDictFile('DataSets/words.txt')
+
+        
+        #This code will emulate a POST command to a RESTful API, 200 Success, 404 Fail
+        PASSWORD = "12345678"
+        
+
+        for i in self.popularPassword:
+            if i == PASSWORD:
+                return 200
+        
+        for i in self.wordList:
+            if i == PASSWORD:
+                return 200
+        return 404
 
